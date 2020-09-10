f5289077e7e7eca0e8213e015692f275349d6dff
Artem Kostiuk <postatum@gmail.com>
2020-09-10 12:52:03 +0300

    Trying to connect x-api-summary to x-api-documentation

--- js/custom-components/components.js
+++ js/custom-components/components.js
@@ -1,6 +1,6 @@
 import { html } from 'lit-element';
 import { ApiSummary } from '@api-components/api-summary/src/ApiSummary.js';
-// import { ApiDocumentation } from '@api-components/api-documentation/api-documentation.js';
+import { ApiDocumentation } from '@api-components/api-documentation/src/ApiDocumentation.js';
 
 function replaceComponentsTags (strings) {
   const tags = [
@@ -14,16 +14,57 @@ function replaceComponentsTags (strings) {
   })
 }
 
+
 export class XApiSummary extends ApiSummary {
-
-}
-window.customElements.define('x-api-summary', XApiSummary);
-
-export class XApiDocumentation extends ApiDocumentation {
-  render() {
-    const res = super.render();
-    res.strings = replaceComponentsTags(res.strings)
-    return res
+  _titleTemplate() {
+    const { _apiTitle, titleLevel } = this;
+    if (!_apiTitle) {
+      return '';
+    }
+    return html`
+    <div class="api-title" role="heading" aria-level="${titleLevel}">
+    <label>TEST:</label>
+    <span>${_apiTitle}</span>
+    </div>`;
   }
 }
-window.customElements.define('x-api-documentation', XApiDocumentation);
+window.customElements.define('api-summary', XApiSummary);
+
+
+export class XApiDocumentation extends ApiDocumentation {
+  // Computes documentation title
+  _computeApiTitle() {
+    const { amf } = this;
+    const webApi = this._computeWebApi(amf);
+    return this._getValue(webApi, this.ns.aml.vocabularies.core.name);
+  }
+
+  // Overriden to add documentation title block
+  render() {
+    if (!this.documentationTitle) {
+      this.documentationTitle = this._computeApiTitle();
+    }
+    const { aware } = this;
+    const res = html`<style>${this.styles}</style>
+    ${html`<div id="api-doc-title">${this.documentationTitle}</div>`}
+    ${aware ? html`<raml-aware
+      .scope="${aware}"
+      @api-changed="${this._apiChanged}"></raml-aware>` : ''}
+    ${this._renderServerSelector()}
+    ${this._renderView()}`;
+    res.strings = replaceComponentsTags(res.strings);
+    return res
+  }
+
+  // Overriden to create breadcrumbs in documentation title block
+  _navigationHandler(e) {
+    const { selected, type, endpointId } = e.detail;
+    console.log(selected, type, endpointId);
+    this.documentationTitle = `${this._computeApiTitle()}`
+    if (type === 'method') {
+      this.documentationTitle += ` / ${endpointId}`
+    }
+    super._navigationHandler(e);
+  }
+}
+window.customElements.define('api-documentation', XApiDocumentation);
